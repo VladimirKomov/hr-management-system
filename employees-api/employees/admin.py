fbbb507c803c6ea88a93c895f6b114e475a7f2d4
VladimirKomov <v.e.komov@gmail.com>
2025-06-15 11:17:07 +0300

    Add models, migrations, and admin for employees management
    Introduce `Employee` and `Location` models with related fields and relationships. Create initial and data-populating migrations for these models. Configure Django admin to manage employees and locations with custom display, search, and filter options.

--- employees-api/employees/admin.py
+++ employees-api/employees/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from employees.models import Location, Employee
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("city", "country")
+    search_fields = ("city", "country")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'position', 'specialization', 'location', 'manager')
+    search_fields = ('full_name', 'telegram_nick')
+    list_filter = ('position', 'specialization', 'location')
--- employees-api/employees/models.py
+++ employees-api/employees/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+# Должность - Возможные варианты: Менеджер, Senior-разработчик, Middle-разработчик, Junior-разработчик
+class Position(models.TextChoices):
+    MANAGER = "Менеджер", "Менеджер"
+    SENIOR = "Senior-разработчик", "Senior-разработчик"
+    MIDDLE = "Middle-разработчик", "Middle-разработчик"
+    JUNIOR = "Junior-разработчик", "Junior-разработчик"
+
+
+# Специализация - Возможные варианты: Python, DevOps
+class Specialization(models.TextChoices):
+    PYTHON = "Python", "Python"
+    DEVOPS = "DevOps", "DevOps"
+
+
+# Город - Доступные города: Москва, Санкт-Петербург, Париж
+class City(models.TextChoices):
+    MOSCOW = "Москва", "Москва"
+    SPB = "Санкт-Петербург", "Санкт-Петербург"
+    PARIS = "Париж", "Париж"
+
+
+# Страна - Россия, Франция
+class Country(models.TextChoices):
+    RUSSIA = "Россия", "Россия"
+    FRANCE = "Франция", "Франция"
+
+
+# Рабочее место - Адрес полностью. Город + Страна: Москва, Россия
+class Location(models.Model):
+    city = models.CharField("Город", max_length=50, unique=True)
+    country = models.CharField("Страна", max_length=50)
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+# Конкретный сотрудник - информацию о конкретном котруднике
+class Employee(models.Model):
+    full_name = models.CharField("ФИО полностью", max_length=100)
+    position = models.CharField("Должность", max_length=30, choices=Position.choices)
+    specialization = models.CharField("Специализация", max_length=30, choices=Specialization.choices)
+    location = models.ForeignKey(Location, on_delete=models.PROTECT, verbose_name="Рабочее место")
+    telegram_nick = models.CharField("Ник в Telegram", max_length=50, blank=True, null=True)
+    about = models.TextField("О себе", blank=True, null=True)
+    manager = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='subordinates',
+        verbose_name="Руководитель"
+    )
+
+    def __str__(self):
+        return self.full_name
